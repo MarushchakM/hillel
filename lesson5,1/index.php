8429eb8b7921b123ca6ba2cced32f09a4604f939
MarushchakM <marushchak099@gmail.com>
2023-09-13 14:15:15 +0300

    lesson5.1

--- lesson5,1/index.php
+++ lesson5,1/index.php
@@ -12,12 +12,13 @@ function circleArea(int|float $radius, $pi = M_PI) : int|float {
 
 echo circleArea(7) . PHP_EOL;
 
+
+$numeric = 2;
 /**
  * @param int|float $numeric
  * @param int $deg
  * @return int|float
  */
-$numeric = 2;
 function power(int|float &$numeric, int $deg = 2){
     $numeric **= $deg;
 }
